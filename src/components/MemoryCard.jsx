70797ea7a36dad353bd042ce424c89452cba2e79
Eric Schorling <eric.schorling@yahoo.com>
2020-10-07 11:15:06 -0400

    initial commit, part 1 done

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import './MemoryCard.css'
+export default class MemboryCard extends Component {
+    render (){
+        return (
+            <div>
+                <div className="MemoryCardBack">
+                    <img class="dcLogo" alt="logo" src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"></img>
+                </div>
+            </div>
+        )
+    }
+}
