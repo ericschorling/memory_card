32ba9ab99509e5f7bb7b86469ac9c17e975d9f60
Eric Schorling <eric.schorling@yahoo.com>
2020-10-07 13:33:53 -0400

    completed step 2

--- src/App.js
+++ src/App.js
@@ -2,39 +2,62 @@ import React from 'react';
 import './App.css';
 import MemoryCard from './components/MemoryCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Memory Game</h1>
-        <h2>Match Cards to Win</h2>
-      </header>
-      <div class="toprow">
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
+const generateDeck =()=> {
+  const symbols = [`∆`,` ß`, `£`, `§`,`•`, `$`, `+`, `ø`]
+  let deck = []
+  for (let i = 0; i < 16; i ++){
+    deck = [...deck, {isFlipped:false, Symbol: symbols[i%8]}]
+  }
+  shuffle(deck)
+  console.log(deck)
+  return deck
+}
+
+const shuffle = (plainDeck) => {
+  for (let i = plainDeck.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random() * (i + 1));
+    [plainDeck[i], plainDeck[j]] = [plainDeck[j], plainDeck[i]]
+  }
+  return plainDeck
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+  
+    this.state ={
+      deck: generateDeck(),
+      pickedCards: [],
+    }
+  }
+  pickCard(cardIndex) {
+    
+  }
+  render() {
+    const cardsJSX = this.state.deck.map((card, index)=>{
+      return <MemoryCard symbol={card.symbol} isFlipped={card.isFlipped} key={index}/>
+    })
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Memory Game</h1>
+          <h2>Match Cards to Win</h2>
+        </header>
+        <div className="toprow">
+          {cardsJSX.slice(0,4)}
+        </div>
+        <div className="secondrow">
+          {cardsJSX.slice(4,8)}
+        </div>
+        <div className="thirdrow">
+          {cardsJSX.slice(8,12)}
+        </div>
+        <div className="bottomrow">
+          {cardsJSX.slice(12,16)}
+        </div>
       </div>
-      <div class="secondrow">
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-      </div>
-      <div class="thirdrow">
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-      </div>
-      <div class="bottomrow">
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,11 +1,35 @@
 import React, {Component} from 'react'
 import './MemoryCard.css'
-export default class MemboryCard extends Component {
+export default class MemoryCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            isFlipped: false,
+        }
+    }
+    clickHandler() {
+        this.setState({
+            isFlipped: !this.state.isFlipped
+        }
+        )
+    }
+
     render (){
+        let memoryCardInnerClass = "MemoryCardInner"
+
+        if (this.props.isFlipped){
+            memoryCardInnerClass += " flipped"
+        }
+
         return (
-            <div>
-                <div className="MemoryCardBack">
-                    <img class="dcLogo" alt="logo" src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"></img>
+            <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+                <div className={memoryCardInnerClass}>
+                    <div className="MemoryCardBack">
+                        <img className="dcLogo" alt="logo" src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"></img>
+                    </div>
+                    <div className="MemoryCardFront">
+                        {this.props.symbol}
+                    </div>
                 </div>
             </div>
         )
